d21322290fd7743e7c86c37e88a33af2150e8efe
Jon Dang <jondang@gmail.com>
2015-02-03 22:20:45 -0500

    basic cover animation

--- js/main.js
+++ js/main.js
@@ -33,31 +33,43 @@ var controller = new ScrollMagic({
  * scene1
  *
  */
-var scene1_animateBg = new TimelineMax()
-  .add([
-    TweenMax.to('.grc-section-1-bg', 1, {
-      scale: '2'
-    })
-  ]);
+// var scene1_animateBg = new TimelineMax()
+//   .add([
+//     TweenMax.to('.grc-section-1-bg', 1, {
+//       scale: '2'
+//     })
+//   ]);
 
-var scene1_animateCover = new TimelineMax()
+var scene1_animation = new TimelineMax()
   .add([
-    TweenMax.to('.grc-section-1-book-cover', 1, {
-      rotationY: '180',
-      y: '-38',
-      x: '-10'
+    TweenMax.to('.grc-section-1-book-cover', 0.5, {
+      rotationY: '90'
+    }),
+    TweenMax.to('.grc-section-1-book-cover-back', 0.5, {
+      rotationY: '90'
+    }),
+    TweenMax.to('.grc-section-1-book-cover-top-shadow', 0.5, {
+      rotationY: '90',
+      opacity: '0.5'
+    })
+  ])
+  .add([
+    TweenMax.to('.grc-section-1-book-cover', 0.01, {
+      opacity: '0'
+    })
+  ])
+  .add([
+    TweenMax.to('.grc-section-1-book-cover-back', 0.5, {
+      rotationY: '180'
     }),
     TweenMax.to('.grc-section-1-book-cover-shadow', 0.5, {
       width: '0%'
-    }),
-    TweenMax.to('.grc-section-1-book-cover-container', 1, {
-      scale: '2'
     })
   ]);
 
 var scene1 = new ScrollScene({duration: 2000})
   .setPin(".grc-section-1")
-  .setTween([scene1_animateBg, scene1_animateCover]);
+  .setTween([scene1_animation]);
 
 
 
