d5bfac3df854a02f6649eae15c03560ae72eade6
Jon Dang <jondang@gmail.com>
2015-02-28 12:56:11 -0500

    s7 ie fix

--- js/main.js
+++ js/main.js
@@ -58,6 +58,11 @@ var scene1_trigger      = false,
     scene8_trigger      = false,
     sceneend_trigger    = false;
 
+// scroll
+var $window = $(window);
+var scrollTime = 0.6;
+var scrollDistance = 150;
+
 
 /* Platform Detection | @detect
    ========================================================================== */
@@ -67,6 +72,10 @@ window.mobilecheck = function() {
   (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
 };
+
+if (navigator.appVersion.indexOf("Win")!=-1) {
+  $('html').addClass("ie");
+}
 
 /* Social | @social
    ========================================================================== */
@@ -127,6 +136,28 @@ $('#grc-tw-share').on('click', function() {
   shareTwitter();
 });
 
+/* GSAP Scroll | @scroll
+   ========================================================================== */
+
+// $(function(){
+
+//   $window.on("mousewheel DOMMouseScroll", function(event){
+
+//     event.preventDefault();
+
+//     var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+//     var scrollTop = $window.scrollTop();
+//     var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+//     TweenMax.to($window, scrollTime, {
+//       scrollTo : { y: finalScroll, autoKill:true },
+//         ease: Power1.easeOut,
+//         overwrite: 5
+//       });
+
+//   });
+// });
+
 /* Init ScrollMagic | @sm-init
    ========================================================================== */
 
@@ -151,7 +182,7 @@ var controller = new ScrollMagic();
  * global toggle sound
  */
 
-var buzzPlaying = true;
+var buzzPlaying = false;
 
 $('#grc-toggle-sound').on('click', function() {
 
@@ -364,6 +395,11 @@ TweenMax.set('.grc-section-3-panel-right-bubble', {
   opacity: 0
 });
 
+// manually set height of s3 because of floated elements
+$('.grc-section-3').css({
+  height: windowHeight
+});
+
 var _veh1 = $('.grc-section-3-panel-left-vehicles');
 var _panelRight = $('.grc-section-3-panel-right-bg-repeat');
 var _s3Char = $('.grc-section-3-panel-left-char');
@@ -456,13 +492,17 @@ TweenMax.set(_s4Text, {
   y: '-10%'
 });
 
-
 TweenMax.set('.grc-section-4-bg', {
   scale: 4
 });
 
 TweenMax.set('.grc-section-4-char', {
   scale: 4
+});
+
+// manually set height of s4 because of floated elements
+$('.grc-section-4').css({
+  height: windowHeight
 });
 
 var scene4_animation = new TimelineMax()
@@ -617,9 +657,20 @@ TweenMax.set('.grc-section-6-text-2', {
   x: '-10%'
 });
 
+TweenMax.set('.grc-section-6-bubble', {
+  opacity: 0
+});
+
+TweenMax.set('.grc-section-6-char', {
+  opacity: 0,
+  x: '200px',
+});
+
 var _roofLine = $('.grc-section-6-roof-line');
 var _s6Text1 = $('.grc-section-6-text-1');
 var _s6Text2 = $('.grc-section-6-text-2');
+var _s6Bubble = $('.grc-section-6-bubble');
+var _s6Char = $('.grc-section-6-char');
 
 // var frameWidth = 100, numCols = 12;
 // var steppedEase = new SteppedEase(numCols);
@@ -654,6 +705,22 @@ var scene6_animation = new TimelineMax({ delay: 2 })
     TweenMax.to('.grc-section-6-graph-animation', 1, {
       backgroundPosition: "-" + s6GraphWidth + "px 0px",
       ease: s6SteppedEase
+    })
+  ])
+  .add([
+    TweenMax.to(_s6Char, 3, {
+      opacity: 1,
+      x: '270px',
+    })
+  ])
+  .add([
+    TweenMax.to(_s6Bubble, 3, {
+      opacity: 1
+    })
+  ])
+  .add([
+    TweenMax.to(_s6Bubble, 3, {
+      opacity: 1
     })
   ]);
 
@@ -696,7 +763,7 @@ var s7Sound = new buzz.sound('sounds/chalk-loop');
 $('.grc-section-7-main-equation').hide();
 $('#s7-svg').show();
 $('.grc-section-7-equation-1, .grc-section-7-equation-2, .grc-section-7-equation-3, .grc-section-7-equation-4, .grc-section-7-equation-5').css({
-  width: '0%',
+  width: '100%',
   overflow: 'hidden'
 });
 
@@ -797,10 +864,15 @@ TweenMax.set(_s7Comet, {
   x: '100%'
 });
 
+// hide end section
+TweenMax.set('.grc-section-end', {
+  opacity: 0
+});
+
 var scene7_animation_bg = new TimelineMax()
   .add([
     TweenMax.to(_s7Chair, 2, {
-      x: '-200%'
+      x: '-10%'
     }),
     TweenMax.to(_s7Char, 10, {
       x: '-10%'
@@ -949,6 +1021,21 @@ var scene7_animation = new TimelineMax()
       x: '-10%',
       opacity: 1
     })
+  ])
+
+  /* begin end section animations */
+
+  // fade out previous stuff
+  .add([
+    TweenMax.to(_s7Text3, 3, {
+      opacity: 0
+    })
+  ])
+  // fade in section end
+  .add([
+    TweenMax.to('.grc-section-end', 3, {
+      opacity: 1
+    })
   ]);
 
 var scene7 = new ScrollScene({
@@ -979,29 +1066,34 @@ scene7.on('enter', function() {
 /* Scene End | @end
    ========================================================================== */
 
+/* Buzz */
+
 var s8Sound = new buzz.sound('sounds/equationscosmos');
 
-var sceneEnd = new ScrollScene({
-  // duration: 4000,
-  triggerElement: '.grc-section-end',
-  triggerHook: '0'
-});
-  // .setPin('.grc-section-7')
-  // .setTween([scene7_animation, scene7_animation_bg]);
+/* Animation Defaults */
 
-sceneEnd.on('enter', function(ev) {
-  if(ev.scrollDirection == "FORWARD") {
-    if (buzzPlaying) s8Sound.play();
-  }
-});
+// TweenMax.set('.grc-section-end', {
+//   opacity: 0
+// });
 
-sceneEnd.on('leave', function(ev) {
-  buzz.all().pause();
-});
+// var sceneEnd = new ScrollScene({
+//   triggerElement: '.grc-section-end',
+//   triggerHook: '0'
+// });
 
-s8Sound.bind('ended', function() {
-  s1Sound.play().loop();
-});
+// sceneEnd.on('enter', function(ev) {
+//   if(ev.scrollDirection == "FORWARD") {
+//     if (buzzPlaying) s8Sound.play();
+//   }
+// });
+
+// sceneEnd.on('leave', function(ev) {
+//   buzz.all().pause();
+// });
+
+// s8Sound.bind('ended', function() {
+//   s1Sound.play().loop();
+// });
 
 /* Controller | @controller
    ========================================================================== */
@@ -1014,8 +1106,8 @@ controller.addScene([
   scene4,
   scene5,
   scene6,
-  scene7,
-  sceneEnd
+  scene7
+  // sceneEnd
 ]);
 
 } // end mobilecheck
