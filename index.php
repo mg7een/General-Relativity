9b965b9efaa4b2deeaf820701458ebccf96c48c6
Martyn Green <mgreen@aaas.org>
2015-03-04 10:41:48 -0500

    merged the updated body text changes on the index page, and added the dummy index placeholder page.

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<!-- metadata here -->
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>General Relativity explained like you've never seen before</title>
+    <meta name="description" content="An interactive comic you don’t need superpowers to understand">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="fb:app_id" content="355911134593287" />
+    <meta property="og:title" content="General Relativity explained like you've never seen before" />
+    <meta property="og:description" content="An interactive comic you don’t need superpowers to understand" />
+    <meta property="og:site_name" content="Science Magazine"/>
+    <meta property="og:type" content="article" />
+    <meta property="og:image" content="http://spark.sciencemag.org/generalrelativity/img/GeneralRelativityMetadata.jpg" />
+    <meta property="article:publisher" content="http://facebook.com/sciencenow" />
+    <meta property="og:url" content="http://spark.sciencemag.org/generalrelativity/?utm_campaign=og_meta" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@sciencemagazine" />
+    <meta name="twitter:creator" content="@sciencemagazine" />
+    <meta name="twitter:title" content="General Relativity explained like you've never seen before" />
+    <meta name="twitter:description" content="An interactive comic you don’t need superpowers to understand" />
+    <meta name="twitter:url" content="http://spark.sciencemag.org/generalrelativity/" />
+    <meta name="twitter:image:src" content="http://spark.sciencemag.org/generalrelativity/img/GeneralRelativityMetadata.jpg" />
+
+    <!-- PRODUCTION DTM SCRIPT-->
+    <script src="//assets.adobedtm.com/926659a1689cc9fca96760b67cca714ae48f0584/satelliteLib-a9d46808a35b7b8034adfab1d8207407222304d5-staging.js"></script>    
+
+</head>
+<body style="background-color:#163758; margin: 0; padding: 0;">
+
+	<img src="img/GeneralsProfile.jpg" style="width:100%;">
+    <script type="text/javascript">_satellite.pageBottom();</script>
+
+</body>
+</html>
